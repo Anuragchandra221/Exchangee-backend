10e0ecc62f04ac07992e8c72f0fc382f7259063d
Anuragchandra221 <pothaluranurag2002@gmail.com>
2023-06-26 12:53:54 +0530

    added requirements.txt

--- exchangee_backend/settings.py
+++ exchangee_backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-xej(8^^5y!h9^h+l#gbg96t0(+i8url_(z_)thn_12*3dh_z%g"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
